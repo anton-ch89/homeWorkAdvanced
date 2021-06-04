e7de8fd0a33ee95f381434469106c01d0ef11f8b
Anton Chudinov <anton.ch89@gmail.com>
2021-06-04 17:44:41 +0500

    Урок 7

--- script.js
+++ script.js
@@ -1,83 +1,19 @@
-'use strict'; 
-
-//    Урок 2
-
-let num = 266219;
-let result = 1;
-
-num = num.toString().split('');
+'use strict';
+let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+let today = new Date().getDay()-1;
 
 
-for (let i = 0; i < num.length; i++) {
-    result *= num[i];
+
+week.forEach ((e, i) => {
+  if (today === i) {
+      if (e === 'Суббота' || e === 'Воскресенье') {
+        document.write(`<p><strong><em>${e}</em></strong></p>`);
+      } else {document.write(`<p><strong>${e}</strong></p>`);}
+  } else if (e === 'Суббота' || e === 'Воскресенье') {
+    document.write(`<p><em>${e}</em></p>`);
+  } else {
+    document.write(`<p>${e}</p>`);
+  }
 }
-
-// for (let i of num) {
-//     result *=i;
-// }
-
-result **= 3;
-
-result = result.toString().substr(0, 2);
-console.log('result: ', result);
-
-//   Урок 3
-// Задание 1
-
-let lang;
-let daysRu = 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье';
-let daysEn = 'Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday';
-
-//  Вариант 1
-// lang = 'ru';
-// if (lang === 'ru') {
-//     console.log(daysRu);
-// }
-// else if (lang === 'en') {
-//     console.log(daysEn);
-// }else {
-//     console.log('Значение переменной задано неверно');
-// }
-
-
-
-//  Вариант 2
-
-// lang = 'en';
-// switch (lang) {
-//     case 'ru': 
-//         console.log(daysRu);
-//         break;
-//     case 'en': 
-//         console.log(daysEn);    
-//         break;
-//     default : 
-//         console.log('Значение переменной задано неверно');
-// }
-
-// Вариант 3
-
-let weeks = [
-['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-];
-
-let week = (lang === 'ru') ? weeks[0] : weeks[1];
-console.log(week);
-
-week = (lang === 'en') ? weeks[1] : weeks[0];
-console.log(week);
-
-
-
-
-// Задание 2
-
-let namePerson;
-
-let position = (namePerson === 'Артем') ? 'Директор' :
-(namePerson === 'Максим') ? 'Преподаватель'
-: 'Студент';
-
-console.log(position);
+);
 
