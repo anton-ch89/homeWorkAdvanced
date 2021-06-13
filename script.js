f1d46260379ff12adada405401b2e51427feae26
anton-ch89 <69715191+anton-ch89@users.noreply.github.com>
2021-06-13 11:20:51 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,40 @@
 
 
 
+//   Урок 3
+// Задание 1
+
+let lang;
+let daysRu = 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье';
+let daysEn = 'Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday';
+
+//  Вариант 1
+// lang = 'ru';
+// if (lang === 'ru') {
+//     console.log(daysRu);
+// }
+// else if (lang === 'en') {
+//     console.log(daysEn);
+// }else {
+//     console.log('Значение переменной задано неверно');
+// }
+
+
+
+//  Вариант 2
+
+// lang = 'en';
+// switch (lang) {
+//     case 'ru': 
+//         console.log(daysRu);
+//         break;
+//     case 'en': 
+//         console.log(daysEn);    
+//         break;
+//     default : 
+//         console.log('Значение переменной задано неверно');
+// }
+
 // Вариант 3
 
 let weeks = {
@@ -9,10 +43,9 @@ let weeks = {
 'en' : ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 };
 
-let lang = 'ru';
+lang = 'ru';
 
 document.write(weeks[lang]);
-
 
 
 
